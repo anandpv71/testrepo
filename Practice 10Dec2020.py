0d8580f59e531fa0470e583ffc4d2e6b7f762891
anandpv71 <73775575+anandpv71@users.noreply.github.com>
2022-02-22 13:25:17 +0530

    Add files via upload

---
+++ Practice 10Dec2020.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 10 16:11:59 2020
+
+@author: user
+"""
+
+friends =['Anand','Jahangir','Krishnarao']
+
+for friend in friends :
+    print('Happy New Year:', friend)
+    
+for i in range(len(friends)) :
+    friend = friends(i)
+    print('Happy New Year:', friend)
+    
+some=[1,9,21,10,16]
+9 in some
+10 in some
+17 in some
